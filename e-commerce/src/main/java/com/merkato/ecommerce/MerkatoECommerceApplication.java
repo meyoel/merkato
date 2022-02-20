8d828ef102626286621ad4b819c8ff4a45a70017
meyoel <54244974+meyoel@users.noreply.github.com>
2022-02-19 23:08:23 -0500

    Add files via upload
    Start up project

---
+++ e-commerce/src/main/java/com/merkato/ecommerce/MerkatoECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.merkato.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkatoECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerkatoECommerceApplication.class, args);
+	}
+
+}
---
+++ e-commerce/src/main/java/com/merkato/ecommerce/models/Customer.java
@@ -0,0 +1,65 @@
+package com.merkato.ecommerce.models;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public Customer(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
